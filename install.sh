d1ba8f5a21e8a26be4e912ae6a010e70813a7b24
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-06-14 16:14:32 +0000

    Feat: Added required packages to env.

--- install.sh
+++ install.sh
@@ -11,7 +11,14 @@ echo "Creating lerobot conda env."
 conda create -yn lerobot python=3.10
 
 echo "Installing dependencies into lerobot env."
-conda run -n lerobot conda install -y -c conda-forge ffmpeg==7.1.1 
+conda run -n lerobot conda install -y -c \
+conda-forge \
+ffmpeg==7.1.1 \
+plotly==5.19.0 \
+matplotlib==3.10.0 \
+nbformat==5.10.4 \
+ipywidgets==8.1.5 \
+
 conda run -n lerobot pip install -e ./lerobot
 
 echo "Installing extra smolvla dependencies."
