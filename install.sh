0f1d1afe212e7504a6631462dcb3da42fc860538
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-06-14 20:22:41 +0000

    Feat: Addded hugging face cli to env.

--- install.sh
+++ install.sh
@@ -15,9 +15,10 @@ conda run -n lerobot conda install -y -c \
 conda-forge \
 ffmpeg==7.1.1 \
 plotly==5.19.0 \
-matplotlib==3.10.0 \
 nbformat==5.10.4 \
 ipywidgets==8.1.5 \
+matplotlib==3.10.0 \
+'huggingface_hub[cli]'==0.31.2 \
 
 conda run -n lerobot pip install -e ./lerobot
 
