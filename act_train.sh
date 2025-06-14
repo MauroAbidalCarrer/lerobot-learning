e8c7feaf3247f36f0680fdd0033686452193bca4
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-06-14 20:47:08 +0000

    Feat: Increased number of training steps and added date to the output directory and job name.

--- act_train.sh
+++ act_train.sh
@@ -1,9 +1,14 @@
+#!/bin/bash
+
+date_name=$(date +%Y%m%d_%H%M)
+
 python lerobot/lerobot/scripts/train.py \
   --dataset.repo_id=SafwanAhmad/smol_test_safwan_1 \
   --policy.type=act \
-  --output_dir=outputs/train/act_so101_test \
-  --job_name=act_so101_test \
+  --output_dir=outputs/train/act_so101_test_5k_steps_$date_name \
+  --job_name=act_so101_test_$date_name \
   --policy.device=cuda \
   --wandb.enable=true \
-  --log_freq=1
-
+  --log_freq=20 \
+  --steps=50000 \
+  --save_freq=2000
