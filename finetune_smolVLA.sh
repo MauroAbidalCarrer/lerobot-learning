fdb129d0bd986d6748ff6d256671ab2924201f85
Mauro Abidal Carrer <mauroabidal@yahoo.fr>
2025-06-14 17:22:43 +0000

    Fix: Switched back to the base train script in finetune_smolVLA.sh because the device aware dataset is 'only' ~50% faster.

--- finetune_smolVLA.sh
+++ finetune_smolVLA.sh
@@ -1,9 +1,10 @@
-python my_train.py \
+# python my_train.py \
+python lerobot/lerobot/scripts/train.py \
   --policy.path=lerobot/smolvla_base \
   --dataset.repo_id=SafwanAhmad/smol_test_safwan_1 \
   --batch_size=64 \
   --steps=200 \
-  --output_dir=outputs/train/ \
-  --job_name=SafwanAhmad_smolvla_finetuned_2000steps \
+  --output_dir=outputs/SafwanAhmad_smolvla_finetuned_2000steps_og_train \
+  --job_name=SafwanAhmad_smolvla_finetuned_2000steps_og_train \
   --policy.device=cuda \
   --log_freq=10
